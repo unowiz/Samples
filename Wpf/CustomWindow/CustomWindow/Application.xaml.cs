890ecc7b2049793d0c9e1abc09dfc67819411074
Takashi Shinohara <takashi.shinohara@outlook.com>
2016-05-18 23:38:32 +0900

    フォルダ名を変更して追加

---
+++ Wpf/CustomWindow/CustomWindow/Application.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace Karamem0.Samples.Wpf.CustomWindow {
+
+    public partial class Application : System.Windows.Application { }
+
+}
