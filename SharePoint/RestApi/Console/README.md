0d2c0935ae8ac766557a0d0b29c736936fbdb7e3
Takashi Shinohara <takashi.shinohara@outlook.com>
2016-12-15 23:44:46 +0900

    PowerShellからREST APIを実行するサンプルの追加

--- SharePoint/RestApi/Console/README.md
+++ SharePoint/RestApi/Console/README.md
@@ -6,5 +6,5 @@
 
 * Azure Active Directory にアプリケーションを登録します。
 * Program.cs の client_id の値を取得した値で書き換えます。
-* アプリケーションのアクセス許可に Dyamics CRM Online を追加します。
+* アプリケーションのアクセス許可に SharePoint Online を追加します。
 * Visual Studio 2015 でプロジェクトを実行します。
---
+++ SharePoint/RestApi/PowerShell/spsrestapi.ps1
@@ -0,0 +1,53 @@
+﻿$tenantId = "<tenantid>"
+$resourceUri = "<resourceuri>"
+$clientId = "<clientid>"
+
+# デバイス コードの取得
+$uri = "https://login.microsoftonline.com/" + $TenantId + "/oauth2/devicecode?" + `
+       "resource=" + [System.Uri]::EscapeDataString($resourceUri) + "&" + `
+       "client_id=" + $clientId
+$headers = @{
+    "Accept" = "application/json"
+}
+$result = Invoke-RestMethod -Method "Get" -Uri $uri -Headers $headers
+
+$userCode = $result.user_code
+$deviceCode = $result.device_code
+
+Write-Output $userCode
+Start-Process "https://aka.ms/devicelogin"
+
+Read-Host | Out-Null
+
+# トークンの取得
+$uri = "https://login.microsoftonline.com/" + $TenantId + "/oauth2/token"
+$headers = @{ 
+    "Accept" = "application/json"
+    "Content-Type" = "application/x-www-form-urlencoded"
+}
+$body = "resource=" + [System.Uri]::EscapeDataString($resourceUri) + "&" + `
+        "client_id=" + $clientId + "&" + `
+        "grant_type=device_code&" + `
+        "code=" + [System.Uri]::EscapeDataString($deviceCode)
+
+$result = Invoke-RestMethod -Method "Post" -Uri $uri -Headers $headers -Body $body
+
+$accessToken = $result.access_token
+
+# サイトのタイトルを取得
+$uri = $resourceUri + "/_api/web/title"
+$headers = @{ 
+    "Accept" = "application/json"
+    "Authorization" = "Bearer " + $accessToken
+}
+$result = Invoke-RestMethod -Method "Get" -Uri $uri -Headers $headers
+Write-Output $result.value
+
+# ドキュメントの一覧を取得
+$uri = $resourceUri + "/_api/web/getfolderbyserverrelativeurl('/Shared%20Documents')/files"
+$headers = @{ 
+    "Accept" = "application/json"
+    "Authorization" = "Bearer " + $accessToken
+}
+$result = Invoke-RestMethod -Method "Get" -Uri $uri -Headers $headers
+$result.value | select Name, TimeCreated, TimeLastModified
