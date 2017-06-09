2398b83bb3bf94337e2822250841c1cb5e413019
Takashi Shinohara <takashi.shinohara@outlook.com>
2017-06-09 18:52:07 +0900

    ExcelServiceOnlineサンプルの追加

---
+++ SharePoint/ExcelServiceOnline/source/ExcelServiceOnline/ExcelServiceOnline/BearerClientMessageInspector.cs
@@ -0,0 +1,27 @@
+﻿using Karamem0.Samples.SharePoint.ExcelServiceOnline.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Dispatcher;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Karamem0.Samples.SharePoint.ExcelServiceOnline {
+
+    public class BearerClientMessageInspector : IClientMessageInspector {
+
+        public void AfterReceiveReply(ref Message reply, object correlationState) {
+        }
+
+        public object BeforeSendRequest(ref Message request, IClientChannel channel) {
+            var property = new HttpRequestMessageProperty();
+            property.Headers.Add("Authorization", "Bearer " + Settings.Default.AccessToken);
+            request.Properties[HttpRequestMessageProperty.Name] = property;
+            return null;
+        }
+
+    }
+
+}
